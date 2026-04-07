2771094060db0e2c8efc08dae88e8c3bbd6f88bd
Dmitry Pavlov <dpavlov@entroforce.ru>
2026-04-07 18:33:17 +0300

    other fixes of references

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-biber --tool references.bib --output-file=references-clones.bib --config=clones.conf
+biber --tool references.bib --output-file=references-clones.bib --config=clones.conf --output-legacy-dates
 xelatex main --nouri-encode
 bibtexu main
 bibtexu sec
