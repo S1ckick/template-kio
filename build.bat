3bf20f1962e86ee06408b4a7b384a4b5d8384dd0
Dmitry Pavlov <dpavlov@entroforce.ru>
2026-04-07 18:37:37 +0300

    sec->eng

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 biber --tool references.bib --output-file=references-clones.bib --config=clones.conf --output-legacy-dates
 xelatex main --nouri-encode
 bibtexu main
-bibtexu sec
+bibtexu eng
 xelatex main --nouri-encode
 xelatex main --nouri-encode
