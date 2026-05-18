ee0571e82f5ca1f6655c48594c83cff45f444be1
Maksim Subbotin <maksim@entroforce.ru>
2026-05-18 14:44:14 +0300

    new references

--- build-en.bat
+++ build-en.bat
@@ -1,5 +1,5 @@
 biber --tool references.bib --output-file=references-clones.bib --config=clones.conf --output-legacy-dates
-perl prepend-en.pl references-en.bib > references-en-prefixed.bib
+biber --tool references-en.bib --output-file=references-en-prefixed.bib --config=prepend-en.conf --output-legacy-dates
 xelatex main-en
 bibtexu main-en
 bibtexu eng
--- prepend-en.pl
+++
@@ -1,7 +0,0 @@
-use strict;
-use warnings;
-
-while (<>) {
-    s/^(\@[a-zA-Z]+\{)([^,\s]+)/$1EN-$2/;
-    print;
-}
